db5682ffb21f6a8f62f8f0fbe0214ba775fee602
Alex Falkowski <alexrfalkowski@gmail.com>
2024-05-18 21:55:49 +0200

    feat(feature): verify feature connection

--- cmd/module.go
+++ cmd/module.go
@@ -3,9 +3,7 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/compressor"
-	"github.com/alexfalkowski/go-service/crypto"
 	"github.com/alexfalkowski/go-service/marshaller"
-	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/telemetry/metrics"
 	"github.com/alexfalkowski/servicectl/config"
@@ -14,7 +12,6 @@ import (
 
 // Module for fx.
 var Module = fx.Options(
-	runtime.Module, crypto.Module,
 	compressor.Module, marshaller.Module,
 	telemetry.Module, metrics.Module,
 	config.Module, cmd.Module,
