1b0236d87fbb377d9d94a6535f2983af2bbe280c
Alex Falkowski <alexrfalkowski@gmail.com>
2024-07-06 09:23:12 +0200

    feat(deps): upgraded upgraded github.com/alexfalkowski/go-service to v1.243.0

--- cmd/module.go
+++ cmd/module.go
@@ -2,9 +2,9 @@ package cmd
 
 import (
 	"github.com/alexfalkowski/go-service/cmd"
-	"github.com/alexfalkowski/go-service/compressor"
+	"github.com/alexfalkowski/go-service/compress"
+	"github.com/alexfalkowski/go-service/encoding"
 	"github.com/alexfalkowski/go-service/env"
-	"github.com/alexfalkowski/go-service/marshaller"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/telemetry/metrics"
 	"github.com/alexfalkowski/servicectl/config"
@@ -13,7 +13,7 @@ import (
 
 // Module for fx.
 var Module = fx.Options(
-	compressor.Module, marshaller.Module,
+	compress.Module, encoding.Module,
 	telemetry.Module, metrics.Module,
 	config.Module, cmd.Module,
 	env.Module, fx.Provide(NewVersion),
