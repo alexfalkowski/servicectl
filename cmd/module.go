4296d939f6562cbd654a922f15b68230d791161d
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-08-28 20:24:59 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.282.0 (#168)

--- cmd/module.go
+++ cmd/module.go
@@ -5,6 +5,7 @@ import (
 	"github.com/alexfalkowski/go-service/compress"
 	"github.com/alexfalkowski/go-service/encoding"
 	"github.com/alexfalkowski/go-service/env"
+	"github.com/alexfalkowski/go-service/sync"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/servicectl/config"
 	"go.uber.org/fx"
@@ -12,7 +13,7 @@ import (
 
 // Module for fx.
 var Module = fx.Options(
-	compress.Module, encoding.Module,
+	sync.Module, compress.Module, encoding.Module,
 	telemetry.Module, config.Module, cmd.Module,
 	env.Module, fx.Provide(NewVersion),
 )
