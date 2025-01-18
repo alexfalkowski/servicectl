aaccd763ad0773b4a48077c700271923300e5f2a
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-01-18 09:25:59 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.376.0 (#332)
    https://github.com/alexfalkowski/go-service/pull/1194

--- cmd/crypto/ed25519/module.go
+++ cmd/crypto/ed25519/module.go
@@ -1,12 +1,10 @@
 package ed25519
 
 import (
-	"github.com/alexfalkowski/go-service/crypto"
 	"go.uber.org/fx"
 )
 
 // Module for fx.
 var Module = fx.Options(
-	crypto.Module,
 	fx.Invoke(Start),
 )
--- cmd/module.go
+++ cmd/module.go
@@ -2,10 +2,7 @@ package cmd
 
 import (
 	"github.com/alexfalkowski/go-service/cmd"
-	"github.com/alexfalkowski/go-service/compress"
-	"github.com/alexfalkowski/go-service/encoding"
-	"github.com/alexfalkowski/go-service/env"
-	"github.com/alexfalkowski/go-service/sync"
+	"github.com/alexfalkowski/go-service/module"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/servicectl/config"
 	"go.uber.org/fx"
@@ -13,7 +10,7 @@ import (
 
 // Module for fx.
 var Module = fx.Options(
-	sync.Module, compress.Module, encoding.Module,
-	telemetry.Module, config.Module, cmd.Module,
-	env.Module, fx.Provide(NewVersion),
+	module.Module, telemetry.Module,
+	config.Module, cmd.Module,
+	fx.Provide(NewVersion),
 )
