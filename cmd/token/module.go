39ff4efd79cc29fda75837a7db8d0310aa764925
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-18 09:00:52 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.376.0
    https://github.com/alexfalkowski/go-service/pull/1194

--- cmd/token/module.go
+++ cmd/token/module.go
@@ -1,14 +1,12 @@
 package token
 
 import (
-	"github.com/alexfalkowski/go-service/crypto"
 	"github.com/alexfalkowski/go-service/token"
 	"go.uber.org/fx"
 )
 
 // Module for fx.
 var Module = fx.Options(
-	crypto.Module,
 	token.Module,
 	fx.Invoke(Start),
 )
